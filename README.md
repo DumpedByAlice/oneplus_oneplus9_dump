a3faf6d89a20b8ca6b3dcfac9f3baf22ebb9d9d0
Hendra Manudinata <hendra@manudinata.me>
2022-02-11 15:56:32 +0000

    Add extras for qssi-user 11 RKQ1.201105.002 2111112054 release-keys
    Signed-off-by: Hendra Manudinata <hendra@manudinata.me>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.201105.002 2111112054 release-keys
+- Manufacturer: oneplus
+- Platform: lahaina
+- Codename: OnePlus9
+- Brand: OnePlus
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.201105.002
+- Incremental: 2111112054
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: OnePlus/OnePlus9/OnePlus9:11/RKQ1.201105.002/2111112054:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.201105.002-2111112054-release-keys
+- Repo: oneplus_oneplus9_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat vendor_bootimg/06_dtbdump_OgER.dtb.* 2>/dev/null >> vendor_bootimg/06_dtbdump_OgER.dtb
+rm -f vendor_bootimg/06_dtbdump_OgER.dtb.* 2>/dev/null
+cat system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk.* 2>/dev/null >> system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk
+rm -f system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk.* 2>/dev/null
+cat system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk.* 2>/dev/null >> system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk
+rm -f system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system_ext/priv-app/OPAppCategoryProvider/OPAppCategoryProvider.apk.* 2>/dev/null >> system_ext/priv-app/OPAppCategoryProvider/OPAppCategoryProvider.apk
+rm -f system_ext/priv-app/OPAppCategoryProvider/OPAppCategoryProvider.apk.* 2>/dev/null
+cat system_ext/app/OPBreathMode/OPBreathMode.apk.* 2>/dev/null >> system_ext/app/OPBreathMode/OPBreathMode.apk
+rm -f system_ext/app/OPBreathMode/OPBreathMode.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/reserve/CanvasResources/CanvasResources.apk.* 2>/dev/null >> system/system/reserve/CanvasResources/CanvasResources.apk
+rm -f system/system/reserve/CanvasResources/CanvasResources.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
